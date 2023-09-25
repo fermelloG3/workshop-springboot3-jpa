e082d13f674d890457b8f9582d29b437c4724ecd
ferdemello <110197887+fermelloG3@users.noreply.github.com>
2023-09-25 19:16:13 -0300

    User entity and resource

---
+++ src/main/java/com/fermelloG3/course/resources/UserResource.java
@@ -0,0 +1,22 @@
+package com.fermelloG3.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fermelloG3.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1, "Fernando de Mello", "ferdemello28@gmail.com", "4799985472", "ValeFer@");
+		return ResponseEntity.ok().body(u);
+		
+	}
+	
+
+}
